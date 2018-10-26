663fce8b1ed583c3a680e20b61d8a2de92fb5885
Kathryn Hoesley <kathrynhoesley@Kathryns-MacBook-Pro.local>
2018-10-25 19:33:58 -0700

    progress on FEC

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader"
+//         }
+//       }
+//     ]
+//   }
+// };
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
 module.exports = {
-  module: {
-    rules: [
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module : {
+    loaders : [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015']
         }
       }
     ]
