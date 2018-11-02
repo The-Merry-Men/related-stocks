c52d432679a035e44132990878c9961e15a7dc40
Katie Hoesley <katiehoesley@gmail.com>
2018-11-01 21:11:03 -0700

    fixing css issues with font sizing and weight

--- client/src/components/CompanyPage.jsx
+++ client/src/components/CompanyPage.jsx
@@ -5,15 +5,17 @@ import LeftArrow from '../components/LeftArrow.jsx'
 function CompanyPage(props) {
     let newCompany = props.companies.map((company) =>
         <div className="stock-card">
-            <div className="company-name">{company.companyName}</div>
-            <div className="approval-rating">
-                <i className="fa fa-tag fa-rotate-90 fa-xl"></i>
-                <div className="tooltip">{company.percentageApproved}%
-                    <span className="tooltiptext">{company.percentageApproved}% of analysts agree {company.companyName} is a buy.</span>
+                <div className="company-name">{company.companyName}
+                <div className="approval-rating">
+                    <i className="fa fa-tag fa-rotate-90 fa-xl"></i>
+                    <div className="tooltip"> {company.percentageApproved}%
+                        <span className="tooltiptext">{company.percentageApproved}% of analysts agree {company.companyName} is a buy.</span>
+                    </div>
                 </div>
             </div>
-            <div className="stock-price">${company.currentPrice}.99</div>
-            <div className="price-fluctuation">+{company.percentageChange/100}%</div>
+            <div className="stock-price">${company.currentPrice}.99
+             <div className="price-fluctuation">+{company.percentageChange/100}%</div>
+            </div>
         </div>
     ); 
 
